93d764436a060d661f4b9234cd6f7921f2194f3a
Alexander Orr <alexanderorr@Alexanders-MacBook-Pro.local>
2018-04-17 22:01:24 -0600

    Caption

--- wp-content/themes/clwp-child/layouts/layout-gallery.php
+++ wp-content/themes/clwp-child/layouts/layout-gallery.php
@@ -4,11 +4,12 @@
     if( have_rows('image') ):
       while ( have_rows('image') ) : the_row();
         $image = get_sub_field('image');
+        $add_caption = get_sub_field('add_caption');
         $caption = get_sub_field('caption');        
         $image = $image['url'];
   ?>
     <div class="gallery-image" style="background-image: url(<?= $image; ?>);">
-    <?php if($caption): ?>
+    <?php if($add_caption === 'yes'): ?>
       <div class="caption">
         <div class="caption-toggle">
         <?php include( get_stylesheet_directory() . '/assets/images/icons/prev-white.svg' ); ?>
